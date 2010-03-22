3615be36236c1222ba278e3220aeb9f000799514
eads.josh <eads.josh@1b86ade9-be3c-bbbc-59bc-0e3bcf06fa43>
2010-03-22 03:31:07 +0000

    Dropped modified EasyHook ProcMon files to monitor send()

---
+++ trunk/EasyHook_modified/Examples/FileMonInject/Main.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Runtime.InteropServices;
+using EasyHook;
+
+namespace FileMonInject
+{
+    public class Main : EasyHook.IEntryPoint
+    {
+        FileMon.FileMonInterface Interface;
+        LocalHook CreateFileHook;
+        Stack<String> Queue = new Stack<String>();
+
+        public Main(
+            RemoteHooking.IContext InContext,
+            String InChannelName)
+        {
+            // connect to host...
+            Interface = RemoteHooking.IpcConnectClient<FileMon.FileMonInterface>(InChannelName);
+
+            Interface.Ping();
+        }
+
+        public void Run(
+            RemoteHooking.IContext InContext,
+            String InChannelName)
+        {
+            // install hook...
+            try
+            {
+
+                CreateFileHook = LocalHook.Create(
+                    LocalHook.GetProcAddress("kernel32.dll", "send"),
+                    new Dsend(send_Hooked),
+                    this);
+
+                CreateFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
+            }
+            catch (Exception ExtInfo)
+            {
+                Interface.ReportException(ExtInfo);
+
+                return;
+            }
+
+            Interface.IsInstalled(RemoteHooking.GetCurrentProcessId());
+
+            RemoteHooking.WakeUpProcess();
+
+            // wait for host process termination...
+            try
+            {
+                while (true)
+                {
+                    Thread.Sleep(500);
+
+                    // transmit newly monitored file accesses...
+                    if (Queue.Count > 0)
+                    {
+                        String[] Package = null;
+
+                        lock (Queue)
+                        {
+                            Package = Queue.ToArray();
+
+                            Queue.Clear();
+                        }
+
+                        Interface.OnCreateFile(RemoteHooking.GetCurrentProcessId(), Package);
+                    }
+                    else
+                        Interface.Ping();
+                }
+            }
+            catch
+            {
+                // Ping() will raise an exception if host is unreachable
+            }
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
+        delegate Int32 Dsend(Int32 s, String buf, Int32 len, Int32 flags);
+
+        // just use a P-Invoke implementation to get native API access from C# (this step is not necessary for C++.NET)
+        [DllImport("ws2_32.dll", CharSet = CharSet.Unicode, SetLastError = true, CallingConvention = CallingConvention.StdCall)]
+        public static extern Int32 send(Int32 s, String buf, Int32 len, Int32 flags);
+
+        // this is where we are intercepting all file accesses!
+        static Int32 send_Hooked(Int32 s, String buf, Int32 len, Int32 flags)
+        {
+            
+            try
+            {
+                Main This = (Main)HookRuntimeInfo.Callback;
+
+                lock (This.Queue)
+                {
+                    This.Queue.Push("[" + RemoteHooking.GetCurrentProcessId() + ":" + 
+                        RemoteHooking.GetCurrentThreadId() +  "]: \"" + buf + "\"");
+                }
+            }
+            catch
+            {
+            }
+
+            // call original API...
+            return send(s, buf, len, flags);
+        }
+    }
+}
