53c0be7163ea89ce31111361aa2e307db272788d
pedgett <pedgett@1b86ade9-be3c-bbbc-59bc-0e3bcf06fa43>
2010-04-05 22:56:58 +0000

    added immunity poc script

---
+++ immunity/468poc.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+"""
+script that right now, hooks send/recv  but only really handles send stuff...
+tabwidth = 4, spaces instead of tabs
+
+put in C:\Program Files\Immunity Inc\Immunity Debugger\PyCommands
+
+run with !468poc at the bottom
+"""
+
+__VERSION__ = '0.000000001'
+
+from immlib import *
+
+DESC= "468 project poc"
+
+class hooker(LogBpHook):
+    def __init__(self):
+        LogBpHook.__init__(self)
+
+    def run(self, regs):
+        imm = Debugger()
+        imm.log("------------------------------")
+        
+        # Figure out which hook it is and print it
+        func_name = imm.getKnowledge("%08x" % regs['EIP'])
+        imm.log(func_name)
+        
+        if func_name == "ws2_32.send":
+            # print_buffer returns the size of buffer... quick way to not
+            # do call stacks of the 1 byte message
+            if self.print_buffer(imm, func_name, regs) > 1:
+                self.print_stack(imm, regs)
+    
+    def print_buffer(self, imm, function, regs):
+        if function == "ws2_32.send":
+            # Get the pointer to the buffer
+            buffer_ptr = imm.readMemory(regs['ESP'] + 8, 4)
+            buffer_ptr = struct.unpack("L", buffer_ptr)[0]
+            
+            # Read buffer size
+            size = imm.readMemory(regs['ESP'] + 12, 4)
+            size = struct.unpack("i", size)[0]
+            
+            buffer = imm.readMemory(buffer_ptr, size)
+            imm.log("    buffer pointer: %08x, size: %s.  contents: %s" % (buffer_ptr, size, buffer))
+           
+            # Something real quick to filter out the 1 byte messages
+            return size
+            
+    
+    def print_stack(self, imm, regs):
+        callstack = imm.callStack()
+        for a in callstack:
+            imm.log("Address: %08x - Stack: %08x - Procedure: %s - frame: %08x - called from: %08x" % \
+                (a.address,a.stack,a.procedure,a.frame,a.calledfrom))
+        
+        
+def main(args):
+    imm = Debugger()
+    target = imm.getDebuggedName()
+    module = imm.getModule(target)
+
+    if not module.isAnalysed():
+        imm.analyseCode(module.getCodebase())
+        
+    hook = hooker()
+    func_list = ["ws2_32.send", "ws2_32.recv"]
+    for f in func_list:
+        # Get fn address and add a hook there
+        addr = imm.getAddress(f)
+        hook.add(f, addr)
+        
+        # Store some info in immunity about the address's name for use later
+        imm.addKnowledge("%08x" % addr, f)
+        
+        imm.log("Added hook for %s at %08x" % (f, addr))
+        
+    return "hooks set like a boss"
